3362741505f180eb45c779523e23f8a824cbefec
Kevin Tieu <ktieu@SCT.local>
2016-03-10 17:57:54 -0500

    Completed selection
    need to add sprite for ship 3

---
+++ Assets/scripts/spawnPlayer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spawnPlayer : MonoBehaviour {
+
+	public static int spawnNum;
+	[SerializeField] private GameObject unit1prefab;
+	[SerializeField] private GameObject unit2prefab;
+	[SerializeField] private GameObject unit3prefab;
+
+	// Use this for initialization
+	void Start () {
+		if (spawnNum == 1)
+			Instantiate (unit1prefab);
+		else if (spawnNum == 2)
+			Instantiate (unit2prefab);
+		else if (spawnNum == 3)
+			Instantiate (unit3prefab);
+	}
+}
