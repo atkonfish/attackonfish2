e991b9d97c6da4c6abca64738c43615f538ba346
Kevin Tsai <kevin791129@gmail.com>
2016-04-22 13:39:14 -0400

    Add Scenes
    -added Credits Scene
    -added Controls Scene

---
+++ Assets/scripts/Localization/Scenes/Controls.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Controls : MonoBehaviour {
+
+	[SerializeField] private Text controls;
+	[SerializeField] private Text main;
+	[SerializeField] private Text shoot;
+	[SerializeField] private Text movement;
+	
+	void Start () {
+		controls.text = localization.Instance.getPhrase(12);
+		main.text = localization.Instance.getPhrase(9);
+		shoot.text = localization.Instance.getPhrase(13);
+		movement.text = localization.Instance.getPhrase(14);
+	}
+}
